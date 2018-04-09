d644bae5c8f21e8e9bacffb93bdbd8d4df4af0e5
Daria Taback <dptaback@ucdavis.edu>
2018-04-08 19:17:22 -0700

    new

---
+++ proto1_03/issue1/script.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+
+document.addEventListener("DOMContentLoaded", function() {
+
+
+});
---
+++ proto1_03/script.js
@@ -0,0 +1,105 @@
+   // JavaScript Document
+
+   document.addEventListener("DOMContentLoaded", function(event) {
+     console.log("DOM fully loaded and parsed");
+
+
+     // current id
+     var current = document.getElementById('current');
+
+     // hover over current color
+     current.addEventListener('mouseover', function() {
+       current.style.opacity = 1;
+       current.src = 'images/current.png';
+     });
+
+     current.addEventListener('mouseout', function() {
+       current.style.opacity = 1;
+       current.src = 'images/current-bw.png';
+     });
+
+     // // click on current for info box
+     // current.addEventListener('click', function() {
+     //   aboutcurrent.style.display = "block";
+     // });
+     //
+     // // current info box that displays on click
+     // document.getElementById('aboutcurrent');
+     // var close = document.getElementById('close');
+     //
+     // close.addEventListener('click', function() {
+     //   aboutcurrent.style.display = "none";
+     // });
+
+
+
+
+     // submit id
+     var submit = document.getElementById('submit');
+
+     // hover over submit color
+     submit.addEventListener('mouseover', function() {
+       submit.style.opacity = 1;
+       submit.src = 'images/submit.png';
+     });
+
+     submit.addEventListener('mouseout', function() {
+       submit.style.opacity = 1;
+       submit.src = 'images/submit-bw.png';
+
+     });
+
+
+
+
+
+     // about id
+     var about = document.getElementById('about');
+
+     // hover over about color
+     about.addEventListener('mouseover', function() {
+       about.style.opacity = 1;
+       about.src = 'images/about.png';
+     });
+
+     about.addEventListener('mouseout', function() {
+       about.style.opacity = 1;
+       about.src = 'images/about-bw.png';
+     });
+
+
+
+
+
+     // archive id
+     var outside = document.getElementById('archive');
+
+     // hover over archive color
+     archive.addEventListener('mouseover', function() {
+       archive.style.opacity = 1;
+       archive.src = 'images/archive.png';
+     });
+
+     archive.addEventListener('mouseout', function() {
+       archive.style.opacity = 1;
+       archive.src = 'images/archive-bw.png';
+     });
+
+
+
+
+     // symptom id
+     var picture = document.getElementById('symptom');
+
+     // hover over symptom color
+     symptom.addEventListener('mouseover', function() {
+       symptom.style.opacity = 1;
+       symptom.src = 'images/symptom.png';
+     });
+
+     symptom.addEventListener('mouseout', function() {
+       symptom.style.opacity = 1;
+       symptom.src = 'images/symptom-bw.png';
+     });
+
+   });
